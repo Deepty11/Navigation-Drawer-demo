386ef7df5697694f789c29e209d311ea70ed3654
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2024-03-24 13:24:42 +0600

    Initial Commit

---
+++ NavigationDrawerDemoUITests/NavigationDrawerDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NavigationDrawerDemoUITestsLaunchTests.swift
+//  NavigationDrawerDemoUITests
+//
+//  Created by Rehnuma Reza(Deepty) on 24/3/24.
+//
+
+import XCTest
+
+final class NavigationDrawerDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
